911a2d5f3a5e30d5110fa1602ffc7810f4a9494a
shengnwen <shengwen@live.cn>
2017-03-20 21:29:49 -0400

    finish step-by-step-guide for resource manager

--- resource-manager/index.js
+++ resource-manager/index.js
@@ -7,7 +7,28 @@ function authorize(callback) {
             console.log('authentication failed:', err);
 			return; 
         }
+        if (authClient.createScopedRequired
+            && authClient.createScopedRequired()) {
+            var scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+			authClient = authClient.createScoped(scopes);
+        }
+        callback(authClient);
     });
 }
+
+authorize(function(authClient) {
+    var request = {
+		resource: {},
+		auth: authClient
+    };
+	
+	crm.projects.create(request, (err, response) => {
+		if (err) {
+			console.log('create failed:', err);
+			return;
+		}
+		console.log(JSON.stringify(response, nll, 2));
+	});
+});
 
 console.log('test');
